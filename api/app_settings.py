e334ce8c0df044f86a96fc3c37f5e67b84d64b91
Juncheol Cho <zironycho@neosapience.com>
2022-05-05 11:34:45 +0900

    update python version; mongodb version

--- api/app_settings.py
+++ api/app_settings.py
@@ -1,42 +1,47 @@
 import os
-
-
-def read_secret_key():
-    app_secret = os.environ.get('APP_SECRET_KEY')
-    if not app_secret:
-        with open(os.environ.get('SECRET_KEY_FILE'), 'rt') as f:
-            app_secret = f.read()
-    if not app_secret:
-        raise RuntimeError('cannot read SECRET_KEY_FILE or APP_SECRET_KEY')
-    return app_secret
+from distutils.util import strtobool
 
 
 class Config(object):
-    SECRET_KEY = read_secret_key()
-    MONGO_HOST = os.environ.get('MONGO_HOST', 'mongo')
-    MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'appname')
-    MONGO_USER = os.environ.get('MONGO_USERNAME', '')
-    MONGO_PASS = os.environ.get('MONGO_PASSWORD', '')
-    MONGO_RSNAME = os.environ.get('MONGO_RSNAME', '')
+    SECRET_KEY = os.environ.get('APP_SECRET_KEY')
 
+    MONGO_HOST = os.environ.get('MONGO_HOST', 'mongo')
+    MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'light')
+    MONGO_USER = os.environ.get('MONGO_USER', '')
+    MONGO_PASS = os.environ.get('MONGO_PASS', '')
+    MONGO_RSNAME = os.environ.get('MONGO_RSNAME', '')
+    
+    use_mgo_atlas = MONGO_HOST.startswith('mongodb+srv://')
     mongo_uri_params = []
     if not MONGO_USER:
         MONGO_URI = f'mongodb://{MONGO_HOST}/{MONGO_DBNAME}'
+    elif use_mgo_atlas:
+        mongo_uri_params += ['retryWrites=true', 'w=majority']
+        MONGO_HOST = MONGO_HOST[14:]
+        MONGO_URI = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}/{MONGO_DBNAME}"
     else:
         mongo_uri_params.append('authSource=admin')
         MONGO_URI = f'mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}/{MONGO_DBNAME}'
 
-    if MONGO_RSNAME:
+    if not use_mgo_atlas and MONGO_RSNAME:
         mongo_uri_params.append(f'replicaSet={MONGO_RSNAME}')
 
     MONGO_URI += '?' + '&'.join(mongo_uri_params)
+
+    REDIS_URL = 'redis://:{}@{}:6379/0'.format(
+        os.environ.get('REDIS_PASSWORD', ''),
+        os.environ.get('REDIS_HOST', 'redis'))
+
     _redis_url = 'redis://:{}@{}:6379/0'.format(
         os.environ.get('REDIS_PASSWORD', ''),
         os.environ.get('REDIS_HOST', 'redis'))
 
     BROKER_URL = _redis_url
+
     CELERY_RESULT_BACKEND = _redis_url
     CELERY_TIMEZONE = 'Asia/Seoul'
+    CELERY_ALWAYS_EAGER = strtobool(os.environ.get('CELERY_ALWAYS_EAGER', 'false'))
+    CELERY_ENABLE_UTC = False
 
 
 class ConfigDebug(Config):
